43baf0a2bfccf13860fbc80e1a53ce1fcbaae3af
Unknown <unknown@example.com>
2021-12-24 16:37:54 -0400

    [update] Implementacion de Manejador de peticiones y conexion con la db

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/luisalfredosv/twiter/db"
+	"github.com/luisalfredosv/twiter/handlers"
+)
+
+func main() {
+	if !db.CheckConnection() {
+		log.Fatal("No hay conexión con la base de datos")
+		return
+	}
+
+	handlers.Handlers()
 }
